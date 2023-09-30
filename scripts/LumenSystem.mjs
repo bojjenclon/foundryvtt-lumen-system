d621a09ea15882a9602704a6167b102ce3637383
Cornell Daly <bojjenclon@gmail.com>
2023-09-29 22:40:36 -0400

    Improve action bar rendering

--- scripts/LumenSystem.mjs
+++ scripts/LumenSystem.mjs
@@ -103,145 +103,6 @@ export class LumenSystem {
       .querySelector('html')
       .classList
       .add('t-dark')
-    
-    if (!game.user.isGM) {
-      return
-    }
-
-    const chatSidebar = document.querySelector('.chat-sidebar')
-    const actionBar = await renderTemplate('systems/lumen/templates/partials/action-bar.hbs')
-    chatSidebar.insertAdjacentHTML('afterbegin', actionBar)
-    
-    $(chatSidebar)
-      .find('.show-dialog')
-      .click(evt => {
-        evt.preventDefault()
-        LumenSystem.showEssenceDialog()
-      })
-
-    $(chatSidebar)
-      .find('.add-essence')
-      .click(async (evt) => {
-        evt.preventDefault()
-        
-        const essenceTables = await game.tables.filter(t => t.folder.name === 'Essence')
-        const options = essenceTables.map(et => `<option value="${et.id}">${et.name}</option>`)
-        
-        let amount = 0
-        let tableId = ''
-        
-        await Dialog.prompt({
-          title: 'Generate Essence',
-          img: 'icons/containers/bags/sack-leather-brown-green.webp',
-          content: [
-            `<p style="text-align: center;"><select name="table">${options.join('')}</select></p>`,
-            `<p><input name="amount" type="number" min="1" step="1" value="1"></p>`,
-          ].join(''),
-          render: (html) => {
-            setTimeout(
-              () => {
-                const input = html.find('input[name="amount"]')
-                input.focus()
-                input.select()
-              },
-              0
-            )
-          },
-          callback: (html) => {
-            amount = html.find('input[name="amount"]').val()
-            tableId = html.find('select[name="table"]').val()
-          }
-        })
-        
-        if (amount < 1 || tableId.length === 0) {
-          return
-        }
-
-        const table = game.tables.find(t => t.id === tableId)
-        const { results } = await table.drawMany(amount, { displayChat: false })
-        
-        for (let res of results) {
-          const { text } = res
-          switch (text) {
-            case 'Health':
-              LumenSystem.addEssence('health', 1)
-              break
-
-            case 'Energy':
-              LumenSystem.addEssence('energy', 1)
-              break
-          }
-        }
-      })
-
-    $(chatSidebar)
-      .find('.clear-essence')
-      .click(evt => {
-        evt.preventDefault()
-        LumenSystem.clearEssence()
-      })
-
-    $(chatSidebar)
-      .find('.apply-essence')
-      .click(async (evt) => {
-        evt.preventDefault()
-
-        const clientStorage = game.settings.storage.get('client')
-        const essence = JSON.parse(clientStorage.getItem('essence') || '[]')
-        
-        for (let idx = essence.length - 1; idx >= 0; idx--) {
-          const ess = essence[idx]
-          const { claimed } = ess
-          const claimedIds = Object.keys(claimed)
-
-          const amountClaimed = Object
-            .values(claimed)
-            .reduce((accum, val) => accum + (val ? 1 : 0), 0)
-          if (amountClaimed === 0) {
-            continue
-          }
-          
-          const addEssence = async (id) => {
-            const user = game.users.get(id)
-            if (!user) {
-              return
-            }
-
-            const { character } = user
-            if (!character) {
-              return
-            }
-            
-            let essProp = ''
-            if (ess.is.health) {
-              essProp = 'health'
-            } else if (ess.is.energy) {
-              essProp = 'energy'
-            }
-            const propPath = `system.${essProp}.value`
-            await character.update({ [propPath]: getProperty(character, propPath) + 1 })
-
-            LumenSystem.removeEssence(idx)
-          }
-          
-          let randoIds = []
-          for (let id of claimedIds) {
-            if (amountClaimed === 1) {
-              await addEssence(id)
-            } else {
-              randoIds.push(id)
-            }
-          }
-          
-          const percent = 1.0 / randoIds.length
-          for (let id of randoIds) {
-            if (Math.random() < percent) {
-              addEssence(id)
-              break
-            }
-          }
-        }
-      })
   }
   
   static socketlib() {
@@ -318,6 +179,150 @@ export class LumenSystem {
     LumenSystem.socket = socket
   }
   
+  static async renderSidebarTab(_dir, html, _opts) {
+    if (!game.user.isGM) {
+      return
+    }
+    
+    if (html.find('.action-bar').length) {
+      return
+    }
+  
+    const actionBar = await renderTemplate('systems/lumen/templates/partials/action-bar.hbs')
+    html.prepend(actionBar)
+    
+    html
+      .find('.show-dialog')
+      .click(evt => {
+        evt.preventDefault()
+        LumenSystem.showEssenceDialog()
+      })
+  
+    html
+      .find('.add-essence')
+      .click(async (evt) => {
+        evt.preventDefault()
+        
+        const essenceTables = await game.tables.filter(t => t.folder.name === 'Essence')
+        const options = essenceTables.map(et => `<option value="${et.id}">${et.name}</option>`)
+        
+        let amount = 0
+        let tableId = ''
+        
+        await Dialog.prompt({
+          title: 'Generate Essence',
+          img: 'icons/containers/bags/sack-leather-brown-green.webp',
+          content: [
+            `<p style="text-align: center;"><select name="table">${options.join('')}</select></p>`,
+            `<p><input name="amount" type="number" min="1" step="1" value="1"></p>`,
+          ].join(''),
+          render: (html) => {
+            setTimeout(
+              () => {
+                const input = html.find('input[name="amount"]')
+                input.focus()
+                input.select()
+              },
+              0
+            )
+          },
+          callback: (html) => {
+            amount = html.find('input[name="amount"]').val()
+            tableId = html.find('select[name="table"]').val()
+          }
+        })
+        
+        if (amount < 1 || tableId.length === 0) {
+          return
+        }
+  
+        const table = game.tables.find(t => t.id === tableId)
+        const { results } = await table.drawMany(amount, { displayChat: false })
+        
+        for (let res of results) {
+          const { text } = res
+          switch (text) {
+            case 'Health':
+              LumenSystem.addEssence('health', 1)
+              break
+  
+            case 'Energy':
+              LumenSystem.addEssence('energy', 1)
+              break
+          }
+        }
+      })
+  
+    html
+      .find('.clear-essence')
+      .click(evt => {
+        evt.preventDefault()
+        LumenSystem.clearEssence()
+      })
+  
+    html
+      .find('.apply-essence')
+      .click(async (evt) => {
+        evt.preventDefault()
+  
+        const clientStorage = game.settings.storage.get('client')
+        const essence = JSON.parse(clientStorage.getItem('essence') || '[]')
+        
+        for (let idx = essence.length - 1; idx >= 0; idx--) {
+          const ess = essence[idx]
+          const { claimed } = ess
+          const claimedIds = Object.keys(claimed)
+  
+          const amountClaimed = Object
+            .values(claimed)
+            .reduce((accum, val) => accum + (val ? 1 : 0), 0)
+          if (amountClaimed === 0) {
+            continue
+          }
+          
+          const addEssence = async (id) => {
+            const user = game.users.get(id)
+            if (!user) {
+              return
+            }
+  
+            const { character } = user
+            if (!character) {
+              return
+            }
+            
+            let essProp = ''
+            if (ess.is.health) {
+              essProp = 'health'
+            } else if (ess.is.energy) {
+              essProp = 'energy'
+            }
+            const propPath = `system.${essProp}.value`
+            await character.update({ [propPath]: getProperty(character, propPath) + 1 })
+  
+            LumenSystem.removeEssence(idx)
+          }
+          
+          let randoIds = []
+          for (let id of claimedIds) {
+            if (amountClaimed === 1) {
+              await addEssence(id)
+            } else {
+              randoIds.push(id)
+            }
+          }
+          
+          const percent = 1.0 / randoIds.length
+          for (let id of randoIds) {
+            if (Math.random() < percent) {
+              addEssence(id)
+              break
+            }
+          }
+        }
+      })
+  }
+  
   static addEssence(type, amount) {
     const { socket } = LumenSystem 
     socket.executeForEveryone('addEssence', type, amount)
@@ -352,3 +357,5 @@ export class LumenSystem {
 Hooks.once('init', LumenSystem.init)
 Hooks.once('ready', LumenSystem.ready)
 Hooks.once('socketlib.ready', LumenSystem.socketlib)
+
+Hooks.on('renderSidebarTab', LumenSystem.renderSidebarTab)
